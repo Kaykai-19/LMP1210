91c4b60a3792c48920d61be955429abed91e1522
Ikeade Adeyinka <ikeade@cedar5.cedar.computecanada.ca>
2023-04-04 11:56:39 -0700

    some changes idek anymore

--- job-script.sh
+++ job-script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --mem=4G
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=8
 #SBATCH --time=4:00:00
-#SBATCH --gpus-per-node=p100:4
-#SBATCH --ntasks-per-node=24
-#SBATCH --exclusive
-#SBATCH --account=rrg-wanglab
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --account=def-wanglab-ab
 #SBATCH --output=%N-%j.out
 #SBATCH --mail-user=ike.adeyinka@mail.utoronto.ca
 #SBATCH --mail-type=ALL
