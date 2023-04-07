1b08fc892b637dbabdffb5c6cdee71fbe3efafcb
Ikeade Adeyinka <ikeade@cedar1.cedar.computecanada.ca>
2023-04-07 06:23:10 -0700

    Added the efficient job script

---
+++ e_net/job-efficient.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --mem=16G
+#SBATCH --cpus-per-task=16
+#SBATCH --time=4:00:00
+#SBATCH --job-name="inception_test"
+#SBATCH --gres=gpu:4
+#SBATCH --account=def-wanglab-ab
+#SBATCH --output=%N-%j.out
+#SBATCH --mail-user=ike.adeyinka@mail.utoronto.ca
+#SBATCH --mail-type=ALL
+
+
+module load cuda cudnn
+source tensorflow/bin/activate
+python gpuEfficient.py
